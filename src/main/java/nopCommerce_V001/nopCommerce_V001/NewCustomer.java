d275f85eb9701bc25282fdd915d6138c1009bc11
roopa <rupe700@gmail.com>
2019-05-15 01:37:32 +0530

    added one file search_cust file and modified new one

---
+++ src/main/java/nopCommerce_V001/nopCommerce_V001/NewCustomer.java
@@ -0,0 +1,16 @@
+package nopCommerce_V001.nopCommerce_V001;
+
+public class NewCustomer {
+
+	
+	public static void m1() {
+		System.out.println("m1() is called");
+	}
+	
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
---
+++ src/test/java/nopCommerce_V001/nopCommerce_V001/TC_Search_Customer_005.java
@@ -0,0 +1,5 @@
+package nopCommerce_V001.nopCommerce_V001;
+
+public class TC_Search_Customer_005 {
+
+}
